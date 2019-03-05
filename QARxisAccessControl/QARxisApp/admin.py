d26629303b886b2154afe198cd8e89bc29a58908
Alejandro Ferro <alejandroferro.1988@gmail.com>
2019-03-05 20:21:09 +0100

    Custom admin page

--- QARxisAccessControl/QARxisApp/admin.py
+++ QARxisAccessControl/QARxisApp/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Event, Intercom, ActionCommand, Door, AccessCode, Route, Person, Location, Image, Sound, State, ImageUploader
 
+#ModelAdmin clases
+
+class IntercomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'description')
+
+class ActionCommandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'action_uri', 'description')
+
+class DoorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'intercom')
+
 # Register your models here.
-admin.site.register(Intercom)
-admin.site.register(ActionCommand)
-admin.site.register(Door)
+admin.site.register(Intercom, IntercomAdmin)
+admin.site.register(ActionCommand, ActionCommandAdmin)
+admin.site.register(Door, DoorAdmin)
 admin.site.register(AccessCode)
 admin.site.register(Route)
 admin.site.register(Person)
