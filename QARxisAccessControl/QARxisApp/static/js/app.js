8b9b18cbd2527f6d83daa7beed2c6295e03af130
AlejandroFerroBejerano <alejandroferro.1988@gmail.com>
2019-03-12 04:21:18 +0100

    Added basic sort functionality

--- QARxisAccessControl/QARxisApp/static/js/app.js
+++ QARxisAccessControl/QARxisApp/static/js/app.js
@@ -1,67 +1,73 @@
 /* -*- mode: js; coding: utf-8 -*- */
-var app= new Vue({
+const app = new Vue({
    el: '#pending_events',
    delimiters: ['[[',']]'],
    data: {
-   events: [],
-   event_status: null,
-   sortKey: 'name',
-   search: '',
-   loading: false,
-   currentEvent: {},
-   message: null,
-   newEvent: { 'event_date': null, 'event_time': null, 'event_kind': null, 'event_des': null, 'event_location': null, 'event_status': null },
-   timer: 3000
- },
- mounted: function() {
-   this.getEvents();
-   setInterval(function() {
+     events: [],
+     event_status: null,
+     currentSort: 'time',
+     currentSortDir: 'asc',
+     search: '',
+     currentEvent: {},
+     message: null,
+     newEvent: { 'event_date': null, 'event_time': null, 'event_kind': null, 'event_des': null, 'event_location': null, 'event_status': null },
+     timer: 10000
+   },
+   /*mounted: function() {
      this.getEvents();
-   }.bind(this), this.timer);
- },
- methods: {
-  getEvents: function() {
-  this.loading = true;
-  this.$http.get('/api/').then((response) => {
-        this.events = response.data;
-        this.loading = false;
-      }).bind(this, this.timer);
- },
- getEvent: function(id) {
-  this.loading = true;
-  this.$http.get('/api/' + id)
-      .then((response) => {
-        this.currentEvent = response.data;
-        this.loading = false;
-      })
-      .catch((err) => {
-        this.loading = false;
-        console.log(err);
-      })
- },
- getStatus: function(id) {
-   this.loading = true;
-   this.$http.get('/api/state/' + id)
-       .then((response) => {
-         this.event_status = response.data;
-         this.loading = false;
-       })
-       .catch((err) => {
-         this.loading = false;
-         console.log(err);
-       })
- },
- getDate: function(timestamp){
-   return timestamp.split('T')[0]
- },
- getTime: function(timestamp){
-   return timestamp.split('T')[1].split('+')[0]
- },
- sortBy: function(sortKey){
-   this.reverse = (this.sortKey == sortKey) ? ! this.reverse: false;
-   this.sortKey = sortKey;
- }
-},
-computed: {
+     setInterval(function() {
+       this.getEvents();
+     }.bind(this), this.timer);
+   },*/
+   created:function() {
+     this.getEvents();
+   },
+   methods: {
+     getEvents:function() {
+       this.$http.get('/api/').then((response) => {
+            this.events = response.data;
+          }).bind(this);
+      },
+      getEvent:function(id) {
+        this.$http.get('/api/' + id)
+          .then((response) => {
+            this.currentEvent = response.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      },
+      getStatus:function(id) {
+        this.$http.get('/api/state/' + id)
+          .then((response) => {
+            this.event_status = response.data;
+          })
+        .catch((err) => {
+          console.log(err);
+        })
+      },
+      getDate:function(timestamp){
+        return timestamp.split('T')[0]
+      },
+      getTime:function(timestamp){
+        return timestamp.split('T')[1].split('+')[0]
+      },
+      sort:function(sortKey){
+        if(sortKey === this.currentSort){
+          this.currentSortDir = this.currentSortDir === 'asc'?'desc':'asc';
+        }
+        this.currentSort = sortKey;
+      },
+   },
+  computed: {
+    sortedEvents:function(){
+      return this.events.sort((a,b) => {
+        let modifier = 1;
+        if (this.currentSortDir === 'desc') modifier = -1;
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+        return 0;
+      });
+    },
   }
 });
